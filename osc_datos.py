d2fc5cc83fa042f5e02bc5809260e58e5085f445
Juan Manuel Piña <23lagunasmentales@gmail.com>
2022-06-29 01:31:04 -0500

    Osc funcionando con datos ordenados

--- osc_datos.py
+++ osc_datos.py
@@ -10,12 +10,12 @@ OSC_CLIENT = OSCClient(OSC_HOST,OSC_PORT)
 
 def send_osc(list_data):
     ruta = "/data".encode()
-    OSC_CLIENT.send_message(ruta)
-    print('enviando',ruta)
+    OSC_CLIENT.send_message(ruta,list_data)
+    #print('enviando',ruta)
     return
 
 #lectura de archivo
-nombre_archivo = "datos_cuenca/cuencas_limpio.txt"
+nombre_archivo = "datos_cuenca/cuencas_limpias.txt"
 file = open(nombre_archivo, 'rt')
 tabla = []
 lines = file.readlines()
@@ -25,5 +25,37 @@ for l in lines:
     tabla.append(row)
 print("numero de filas en la tabla", len(tabla))
 print("numero de columnas por fila", len(row))
+# imprime archivo
+print ("La tabla del archivo es:")
+print ("------------------------")
+for row in tabla:
+    for col in row:
+        print(col, '\t', end=''), 
+    print('')
+print ('------------------------')
 
+#loop
+
+i = 0
+while (True):
+    in_line = tabla[i]
+    # print(in_line)
+    valores = [0,0,0]
+    try:    
+        valores = [i, int(in_line[0]), float(in_line[2])]
+        
+    except:
+        i=i+1
+        continue
+
+    #Envio de valores
+    send_osc(valores)
+    print(">> ", valores)
+
+    if i < (len(tabla)-1):
+        i = i+1
+    else:
+        i = 0
+    
+    sleep(0.5)
 
